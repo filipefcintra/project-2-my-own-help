5682ac06447e2dc4a5c1a215a3f1918ed75c82a2
rafa-alves-15 <rafaelalves.15@outlook.com>
2021-07-04 21:36:53 -0300

    alteracao

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Homepage from "./components/Homepage";
-
+import Navbar from "./components/NavBar";
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar/>      
         <Route exact path="/" component={Homepage} />
       </BrowserRouter>
     </div>
