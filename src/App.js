b488dc5eb8510bb7db86cdaca108a09de7834994
rafa-alves-15 <rafaelalves.15@outlook.com>
2021-07-05 18:56:50 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import AddNew from "./components/AddNew";
 import Search from "./components/Search";
